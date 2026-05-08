b95ce20e0c0cc632d4a790ee14a92c611442505e
Diego2D_Frontend <frontend.diego2d16@gmail.com>
2026-05-08 14:22:48 -0600

    chore-style: estructutra del navbar agregando cdnjs & estilos globales,

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
 
   return (
-    <h1>Navbar Responsivo</h1>
+    <header>
+      <h1>Navbar</h1>
+
+      <nav>
+        <div>
+          <i class="fa-solid fa-bars"></i>
+        </div>
+
+        <ul>
+          <li>Home</li>
+          <li>Categories</li>
+          <li>Profile</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
