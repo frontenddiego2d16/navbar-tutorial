a8ea5dc0b1d011be953e64d958b32db386c75b7f
Diego2D_Frontend <frontend.diego2d16@gmail.com>
2026-05-08 16:25:46 -0600

    style: diseño mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
+import styles from './App.module.css'
+
 function App() {
 
   return (
-    <header>
-      <h1>Navbar</h1>
+    <header className={styles.containerNavbar}>
+      <h1 className={styles.title}>Navbar</h1>
 
-      <nav>
-        <div>
-          <i class="fa-solid fa-bars"></i>
+
+      <div className={styles.navbarIconOpen}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+
+      <nav className={`${styles.navbar} ${styles.navbarClose}`}>
+
+        <div className={styles.navbarIconClose}>
+          <i className="fa-solid fa-x"></i>
         </div>
 
-        <ul>
-          <li>Home</li>
-          <li>Categories</li>
-          <li>Profile</li>
-          <li>About</li>
+        <ul className={styles.navbarList}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Categories</a></li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">About</a></li>
         </ul>
+
+        <button className={styles.btnHighlight}>
+          Log in
+        </button>
       </nav>
     </header>
   )
 }
 
-export default App;
+export default App; 
