9dcc5284bef39080c7a9e921104b51f23d13bfb1
Diego2D_Frontend <frontend.diego2d16@gmail.com>
2026-05-08 16:28:37 -0600

    feat: agregando useState para abrir el menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
 import styles from './App.module.css'
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className={styles.containerNavbar}>
       <h1 className={styles.title}>Navbar</h1>
 
 
-      <div className={styles.navbarIconOpen}>
+      <div className={styles.navbarIconOpen}
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <i className="fa-solid fa-bars"></i>
       </div>
 
-      <nav className={`${styles.navbar} ${styles.navbarClose}`}>
+      <nav className={`${styles.navbar} ${isOpen ? styles.navbarIconOpen : styles.navbarClose}`}>
 
-        <div className={styles.navbarIconClose}>
+        <div className={styles.navbarIconClose}
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <i className="fa-solid fa-x"></i>
         </div>
 
