4c05a1f3c0647d591f89ccbf9d0437eb121a25f6
Diego2D_Frontend <frontend.diego2d16@gmail.com>
2026-05-18 14:03:18 -0600

    styles: añadiendo estilos con tailwind css

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 
 function Navbar() {
+
   const Links = [
     { id: 1, name: 'Home' },
     { id: 2, name: 'Categories' },
@@ -13,21 +14,25 @@ function Navbar() {
 
   return (
     <>
-      <header>
-        <h2>Navbar</h2>
+      <header className="bg-gray-800 flex justify-between p-5 shadow-lg shadow-secondary relative">
+        <h2 className="drop-shadow-lg drop-shadow-secondary text-2xl">
+          Navbar
+        </h2>
 
-        <div>
+        <div className="drop-shadow-lg drop-shadow-alert text-2xl">
           <i className="fa-solid fa-bars"></i>
         </div>
 
-        <nav>
-          <div>
+        <nav className="p-5 absolute top-0 right-0 w-1/2 h-screen bg-black/80 shadow-lg shadow-accent  flex-col gap-10 hidden"
+        >
+          <div className="flex justify-end drop-shadow-xl drop-shadow-alert text-2xl">
             <i className="fa-solid fa-x"></i>
           </div>
-          <ul>
+
+          <ul className="flex flex-col items-center gap-5 text-lg">
             {
               Links.map(link =>
-                <li key={link.name} >
+                <li key={link.name} className="text-primary text-shadow-2xs text-shadow-secondary">
                   <NavbarList name={link.name} />
                 </li>
               )
